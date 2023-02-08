7e37028d5b070ea81b37a614c7d14bfb03fd2c8f
sanghoonLee <strangehoon2@gmail.com>
2023-02-08 18:40:10 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,9 @@
+ascending = [1,2,3,4,5,6,7,8]
+descending = [8,7,6,5,4,3,2,1]
+data = list(map(int, input().split()))
+if data == ascending:
+    print("ascending")
+elif data == descending:
+    print("descending")
+else:
+    print("mixed")
