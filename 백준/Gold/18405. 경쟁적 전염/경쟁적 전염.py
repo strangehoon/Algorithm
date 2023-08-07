bfb3689de9dd943bda7a6f045fc72e3025e59132
sanghoonLee <strangehoon2@gmail.com>
2023-08-07 22:53:37 +0900

    [Gold V] Title: 경쟁적 전염, Time: 128 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -5,28 +5,26 @@ for i in range(N):
     graph.append(list(map(int, input().split())))
 S, X, Y = map(int, input().split())
 
-queue = deque()
-
-
 # 초기 큐 설정
-for k in range(1, K+1):
-    for i in range(N):
-        for j in range(N):
-            if graph[i][j] == k:
-                queue.append((i,j,graph[i][j]))
-
+data = []
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] != 0:
+            data.append((graph[i][j], i, j))
+data.sort()
+queue = deque(data)
 
 # 동, 서, 남, 북
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 def bfs(queue):
-    x,y,v = queue.popleft()
+    v, x, y = queue.popleft()
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         if 0<=nx<N and 0<=ny<N and graph[nx][ny] == 0:
             graph[nx][ny] = v
-            queue.append((nx,ny,v))
+            queue.append((v, nx, ny))
 
 for i in range(S):
     for j in range(len(queue)):
