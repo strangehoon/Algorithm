e4886f5f37625726d6b4ba4cb070d6bad9321508
sanghoonLee <strangehoon2@gmail.com>
2023-08-16 20:15:53 +0900

    [Silver III] Title: 퇴사, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,17 +1,16 @@
 n = int(input())
 
 data = []
-data.append([0,0])
 for _ in range(n):
     data.append(list(map(int, input().split())))
 
 dp = [0 for _ in range(n+1)]
 
 for i in range(1, n+1):
-    t = data[i][0]
-    p = data[i][1]
-    if dp[i] < dp[i-1]:
-        dp[i] = dp[i-1]
+    t = data[i-1][0]
+    p = data[i-1][1]
     if i + t -1 <= n:
         dp[i+t-1] = max(dp[i+t-1], dp[i-1] + p)
+    if dp[i] < dp[i-1]:
+        dp[i] = dp[i-1]
 print(dp[n])
