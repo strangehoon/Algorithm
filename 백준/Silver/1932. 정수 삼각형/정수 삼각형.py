4f08d4809259e2aa247e61b79b51b4733c2923a3
sanghoonLee <strangehoon2@gmail.com>
2023-08-18 22:59:16 +0900

    Update 정수 삼각형.py

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -5,12 +5,7 @@ for i in range(n):
     tri.append(list(map(int, input().split())))
 
 for i in range(n-2, -1, -1):
-    x = len(tri[i])
-    for j in range(x):
-        first = tri[i+1][j]
-        second = tri[i+1][j+1]
-        if first > second:
-            tri[i][j] = first + tri[i][j]
-        else:
-            tri[i][j] = second + tri[i][j]
+    for j in range(len(tri[i])):
+        tri[i][j] = max(tri[i+1][j] + tri[i][j], tri[i+1][j+1] + tri[i][j])
+
 print(tri[0][0])
